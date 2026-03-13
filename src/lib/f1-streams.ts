c5d588410b6f9d46f939b297622fbdcbc4cd6749
Rabindra Tamang <rabindra.tamang@g-mana.com>
2026-03-13 09:31:36 +0545

    Update url

--- src/lib/f1-streams.ts
+++ src/lib/f1-streams.ts
@@ -4,10 +4,10 @@ export interface F1StreamOption {
 }
 
 export const F1_STREAMS: F1StreamOption[] = [
-  { name: "Sky Moto", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL3NreV9tb3RvL3RyYWNrcy12MWExL3N0eWxlLmNzcw==" },
-  { name: "F1 Italy", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2YxX2l0YS90cmFja3MtdjFhMS9zdHlsZS5jc3M=" },
-  { name: "F1 Spain", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2YxX2VzcC90cmFja3MtdjFhMS9zdHlsZS5jc3M=" },
-  { name: "360p LR", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2ZvbmVfbHIvdHJhY2tzLXYxYTEvc3R5bGUuY3Nz" },
+  { name: "Sky Moto", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2V2ZW50MDEvdHJhY2tzLXYxYTEvc3R5bGUuY3Nz" },
+  { name: "F1 Italy", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2V2ZW50MDQvdHJhY2tzLXYxYTEvc3R5bGUuY3Nz" },
+  { name: "F1 Spain", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2V2ZW50MDUvdHJhY2tzLXYxYTEvc3R5bGUuY3Nz" },
+  { name: "360p LR", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2V2ZW50MDIvdHJhY2tzLXYxYTEvc3R5bGUuY3Nz" },
 ];
 
 function decodeBase64Url(encoded: string): string {
