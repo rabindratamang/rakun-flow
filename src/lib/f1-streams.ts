b4cc6ce5d2614012328088deb85b2269cc97341f
Rabindra Tamang <rabindra.tamang@g-mana.com>
2026-06-07 18:18:54 +0545

    update links

--- src/lib/f1-streams.ts
+++ src/lib/f1-streams.ts
@@ -4,10 +4,10 @@ export interface F1StreamOption {
 }
 
 export const F1_STREAMS: F1StreamOption[] = [
-  { name: "Sky Moto", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2V2ZW50MDEvdHJhY2tzLXYxYTEvc3R5bGUuY3Nz" },
-  { name: "F1 Italy", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2V2ZW50MDQvdHJhY2tzLXYxYTEvc3R5bGUuY3Nz" },
-  { name: "F1 Spain", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2V2ZW50MDUvdHJhY2tzLXYxYTEvc3R5bGUuY3Nz" },
-  { name: "360p LR", url: "aHR0cHM6Ly9kYXNoLnNlcnZlcGxheS5zaXRlL2V2ZW50MDIvdHJhY2tzLXYxYTEvc3R5bGUuY3Nz" },
+  { name: "Stream 1", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvNjI1NjMwYmJlMGIwYWUxMGZjOTIzMmQ4YzkzYTliZmUvaW5kZXgubTN1OA==" },
+  { name: "Stream 2", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvOTdmNjdhOGE1Nzc1MDE2ODZlN2UzYTA5YjFmYjI4MzEvaW5kZXgubTN1OA==" },
+  { name: "F1 Italy", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvYTEwYzY5ZTUxMDhhNjQ2MGY1ZDc2OTBkNDA1ZTllMDEvaW5kZXgubTN1OA==" },
+  { name: "Multi", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvYWUyZmM1YzA0NGM2YTlmOWE5YjcwZmI2OTNiMDQ0NzQvaW5kZXgubTN1OA==" },
 ];
 
 function decodeBase64Url(encoded: string): string {
