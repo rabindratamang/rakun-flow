6d28fe995b782b52fc90f6f5261d1e90da19adfa
Rabindra Tamang <rabindra.tamang@g-mana.com>
2026-06-14 19:05:23 +0545

    Update stream

--- src/lib/f1-streams.ts
+++ src/lib/f1-streams.ts
@@ -4,10 +4,10 @@ export interface F1StreamOption {
 }
 
 export const F1_STREAMS: F1StreamOption[] = [
-  { name: "Stream 1", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvNjI1NjMwYmJlMGIwYWUxMGZjOTIzMmQ4YzkzYTliZmUvaW5kZXgubTN1OA==" },
-  { name: "Stream 2", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvOTdmNjdhOGE1Nzc1MDE2ODZlN2UzYTA5YjFmYjI4MzEvaW5kZXgubTN1OA==" },
-  { name: "F1 Italy", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvYTEwYzY5ZTUxMDhhNjQ2MGY1ZDc2OTBkNDA1ZTllMDEvaW5kZXgubTN1OA==" },
-  { name: "Multi", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvYWUyZmM1YzA0NGM2YTlmOWE5YjcwZmI2OTNiMDQ0NzQvaW5kZXgubTN1OA==" },
+  { name: "Stream 1", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvMmI4NTU0NDM4Y2IzN2Y1ZmZjOGM3OTI5NjJkMmZhYmYvaW5kZXgubTN1OA==" },
+  { name: "Stream 2", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvNDc4MjcyZGUxNDk1ZThmM2VjNWNiZTQwOTE1NmQ5ZWMvaW5kZXgubTN1OA==" },
+  { name: "F1 Italy", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvOTg0ZTM4OWU3NjQ1NWZmNDIxY2Q0ZjJhYTE5Y2UyZDEvaW5kZXgubTN1OA==" },
+  { name: "German", url: "aHR0cHM6Ly9vZTEub3NzZmVlZC5zdG9yZS9vdXQvdjIvYWIzYWViYzI5NDcwZjgzMjlhYmFhMzhhNzJmNDdkYWQvaW5kZXgubTN1OA==" },
 ];
 
 function decodeBase64Url(encoded: string): string {
